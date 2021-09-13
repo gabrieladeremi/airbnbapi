5615c5f83f3ad2febbb11ed8d8b46e28894b2cc2
Aderemi Amos <aderemi.amos@decagon.dev>
2021-09-13 02:02:00 +0100

    Models, migrations, factories, seeders all implemented

---
+++ database/factories/OfficeFactory.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Office;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class OfficeFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Office::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'user_id' => User::factory(),
+            'title' => $this->faker->sentence,
+            'description' => $this->faker->paragraph,
+            'lat' => $this->faker->latitude,
+            'lng' => $this->faker->longitude,
+            'address-line1' => $this->faker->address,
+            'approval_status' => Office::APPROVAL_APPROVED,
+            'hidden' => false,
+            'price_per_day' => $this->faker->numberBetween(10000, 20000),
+            'monthly_discount' => 0
+        ];
+    }
+}
